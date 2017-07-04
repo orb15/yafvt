bdf4189b6979e2f1090e85d7af05004daa704654
orb15 <loumyers67@gmail.com>
2017-07-04 16:49:45 -0400

    Initial commit

---
+++ src/test/java/net/orb15/yafvt/YafvtApplicationTests.java
@@ -0,0 +1,16 @@
+package net.orb15.yafvt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YafvtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
